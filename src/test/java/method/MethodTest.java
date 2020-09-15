ff336ad6581de948fc808e9c6a1c1c9d64d62587
yhh1056 <yhh1056@naver.com>
2020-09-15 20:45:56 +0900

    split

--- src/test/java/method/MethodTest.java
+++ src/test/java/method/MethodTest.java
@@ -2,7 +2,9 @@ package method;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * author {yhh1056}
@@ -11,10 +13,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MethodTest {
 
     @Test
-    void StringBuilderReverse() {
+    void stringBuilderReverse() {
         String string = "abcd";
         StringBuilder sb = new StringBuilder(string);
 
         assertEquals("dcba", String.valueOf(sb.reverse()));
+    }
+
+    @Test
+    void splitString() {
+        String string = "abcdef";
+        String hasSpecialCharacters = "ab:cd,ef";
+
+        assertArrayEquals(new String[]{"a", "b", "c", "d", "e", "f"}, string.split(""));
+        assertArrayEquals(new String[]{"ab", "def"}, string.split("c"));
+
+        assertArrayEquals(new String[]{}, string.split("abcdef"));
+        assertEquals(0, string.split("abcdef").length);
+
+        assertArrayEquals(new String[]{"ab", "cd", "ef"}, hasSpecialCharacters.split("[:,]"));
     }
 }

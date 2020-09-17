cc488c0dd91b0edc153dec237f5125d800cc0335
yhh1056 <yhh1056@naver.com>
2020-09-18 01:57:14 +0900

    배열에서 작은 값부터 인덱스 출력

--- src/test/java/method/MethodTest.java
+++ src/test/java/method/MethodTest.java
@@ -3,6 +3,9 @@ package method;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,5 +35,41 @@ public class MethodTest {
         assertEquals(0, string.split("abcdef").length);
 
         assertArrayEquals(new String[]{"ab", "cd", "ef"}, hasSpecialCharacters.split("[:,]"));
+    }
+
+    @Test
+    void toCharArray() {
+        String string = "hello";
+        char[] chars = new char[]{'h', 'e', 'l', 'l', 'o'};
+
+        assertArrayEquals(chars, string.toCharArray());
+    }
+
+    @Test
+    void intToString() {
+        int[] ints = new int[]{1, 2, 3, 4, 5};
+        String string = "[1, 2, 3, 4, 5]";
+
+        assertEquals(string, Arrays.toString(ints));
+    }
+
+    @Test
+    void arrayFill() {
+        int[] ints = new int[5];
+        Arrays.fill(ints, 3);
+
+        assertArrayEquals(new int[]{3, 3, 3, 3, 3}, ints);
+    }
+
+    @Test
+    void findMaxValueIndex() {
+        //given
+        int[] a = new int[]{1, 6, 2, 7, 8};
+        //when
+        int[] sortedIndices = IntStream.range(0, a.length)
+                .boxed().sorted(Comparator.comparingInt(i -> a[i]))
+                .mapToInt(ele -> ele).toArray();
+        //then
+        assertArrayEquals(new int[]{0, 2, 1, 3, 4}, sortedIndices);
     }
 }
